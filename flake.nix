97a8c30de15d515ce66da7f3a935899f348779b9
pranc1ngpegasus <temma.fukaya@mokmok.dev>
2026-02-04 18:37:21 +0900

    feat: setup nix flake

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+        rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rust
+            pkgs.sccache
+            pkgs.nixfmt
+          ];
+
+          shellHook = ''
+            export RUSTC_WRAPPER=${pkgs.sccache}/bin/sccache
+          '';
+        };
+      }
+    );
+}
